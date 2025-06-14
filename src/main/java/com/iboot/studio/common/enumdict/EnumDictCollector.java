ecd1bacf71caa68e2d86a206a20a72c235a7bda2
tangsc <1419054707@qq.com>
2025-06-14 23:02:12 +0800

    refactor: 重构代码便于维护

--- src/main/java/com/iboot/studio/common/enumdict/EnumDictCollector.java
+++ src/main/java/com/iboot/studio/common/enumdict/EnumDictCollector.java
@@ -40,51 +40,95 @@ public class EnumDictCollector {
   }
 
   private void scanEnums() {
+    // 创建类扫描器
+    ClassPathScanningCandidateComponentProvider scanner = createScanner();
+    
+    // 扫描每个包
+    for (String packageName : scanPackages.split(",")) {
+      scanPackage(scanner, packageName.trim());
+    }
+  }
+
+  private ClassPathScanningCandidateComponentProvider createScanner() {
     ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
     scanner.addIncludeFilter(new AnnotationTypeFilter(EnumDict.class));
+    return scanner;
+  }
 
-    String[] packages = scanPackages.split(",");
-    for (String packageName : packages) {
-      try {
-        Set<Class<?>> enumClasses = scanner.findCandidateComponents(packageName.trim()).stream()
-            .map(beanDefinition -> {
-              try {
-                return Class.forName(beanDefinition.getBeanClassName());
-              } catch (ClassNotFoundException e) {
-                return null;
-              }
-            })
-            .filter(clazz -> clazz != null && clazz.isEnum() && IEnumDict.class.isAssignableFrom(clazz))
-            .collect(Collectors.toSet());
-
-        for (Class<?> enumClass : enumClasses) {
-          EnumDict annotation = enumClass.getAnnotation(EnumDict.class);
-          String value = annotation.value();
-          String key = StringUtils.hasText(value) ? value : enumClass.getName();
-
-          if (enumDictMap.containsKey(key)) {
-            throw new IllegalStateException(
-                String.format("发现重复的EnumDict value='%s'，存在于以下类中：%s", key, enumClass.getName()));
-          }
-
-          EnumDictInfo info = new EnumDictInfo();
-          info.setName(key);
-          info.setDisplayName(annotation.displayName());
-          info.setOptions(
-              Arrays.stream(enumClass.getEnumConstants())
-                  .map(
-                      enumConstant -> {
-                        IEnumDict enumDict = (IEnumDict) enumConstant;
-                        return new EnumOption(enumDict.getValue(), enumDict.getLabel());
-                      })
-                  .collect(Collectors.toList()));
-
-          enumDictMap.put(key, info);
-        }
-      } catch (Exception e) {
-        log.warn("扫描包 {} 时出错", packageName, e);
+  private void scanPackage(ClassPathScanningCandidateComponentProvider scanner, String packageName) {
+    try {
+      // 获取包下的所有候选组件
+      Set<Class<?>> enumClasses = findEnumClasses(scanner, packageName);
+      
+      // 处理每个枚举类
+      for (Class<?> enumClass : enumClasses) {
+        processEnumClass(enumClass);
       }
+    } catch (Exception e) {
+      log.warn("扫描包 {} 时出错", packageName, e);
     }
+  }
+
+  private Set<Class<?>> findEnumClasses(ClassPathScanningCandidateComponentProvider scanner, String packageName) {
+    return scanner.findCandidateComponents(packageName).stream()
+        .map(this::loadClass)
+        .filter(this::isValidEnumDict)
+        .collect(Collectors.toSet());
+  }
+
+  private Class<?> loadClass(org.springframework.beans.factory.config.BeanDefinition beanDefinition) {
+    try {
+      return Class.forName(beanDefinition.getBeanClassName());
+    } catch (ClassNotFoundException e) {
+      return null;
+    }
+  }
+
+  private boolean isValidEnumDict(Class<?> clazz) {
+    return clazz != null && clazz.isEnum() && IEnumDict.class.isAssignableFrom(clazz);
+  }
+
+  private void processEnumClass(Class<?> enumClass) {
+    // 获取注解信息
+    EnumDict annotation = enumClass.getAnnotation(EnumDict.class);
+    String key = getEnumDictKey(annotation, enumClass);
+
+    // 检查重复
+    checkDuplicateKey(key, enumClass);
+
+    // 创建字典信息
+    EnumDictInfo info = createEnumDictInfo(annotation, enumClass);
+    enumDictMap.put(key, info);
+  }
+
+  private String getEnumDictKey(EnumDict annotation, Class<?> enumClass) {
+    return StringUtils.hasText(annotation.value()) ? annotation.value() : enumClass.getName();
+  }
+
+  private void checkDuplicateKey(String key, Class<?> enumClass) {
+    if (enumDictMap.containsKey(key)) {
+      throw new IllegalStateException(
+          String.format("发现重复的EnumDict value='%s'，存在于以下类中：%s", key, enumClass.getName()));
+    }
+  }
+
+  private EnumDictInfo createEnumDictInfo(EnumDict annotation, Class<?> enumClass) {
+    EnumDictInfo info = new EnumDictInfo();
+    info.setName(getEnumDictKey(annotation, enumClass));
+    info.setDisplayName(annotation.displayName());
+    info.setOptions(createEnumOptions(enumClass));
+    return info;
+  }
+
+  private List<EnumOption> createEnumOptions(Class<?> enumClass) {
+    return Arrays.stream(enumClass.getEnumConstants())
+        .map(this::createEnumOption)
+        .collect(Collectors.toList());
+  }
+
+  private EnumOption createEnumOption(Object enumConstant) {
+    IEnumDict enumDict = (IEnumDict) enumConstant;
+    return new EnumOption(enumDict.getValue(), enumDict.getLabel());
   }
 
   public List<EnumDictInfo> getAllEnumDicts() {
